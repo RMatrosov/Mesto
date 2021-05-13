114f55b0e2c943fe896141e7d3ce8a6adb8442df
Roman Matrosov <r.a.matrosov@gmail.com>
2021-05-13 17:57:39 +0300

    first iteration bug fix

--- scripts/script.js
+++ scripts/script.js
@@ -2,26 +2,21 @@ const redactBtn = document.querySelector('.profile__button');
 const popup = document.querySelector('.popup');
 const closeBtn = document.querySelector('.popup__button-close');
 const formElement = document.querySelector('.popup__container');
-const nameInput = document.querySelector('.popup__name');
-const jobInput = document.querySelector('.popup__job');
+const nameInput = document.querySelector('#name');
+const jobInput = document.querySelector('#job');
 const profileName = document.querySelector('.profile__name')
 const profileJob = document.querySelector('.profile__job');
 
-redactBtn.addEventListener('click', popupOpen);
-closeBtn.addEventListener('click', popupClose);
-formElement.addEventListener('submit', formSubmitHandler);
-
 function popupOpen() {
     popup.classList.add('popup_opened');
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
 }
 
 function popupClose(evt) {
     evt.preventDefault();
     popup.classList.remove('popup_opened');
-    nameInput.value = profileName.textContent;
-    jobInput.value = profileJob.textContent;
 }
-
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
@@ -32,5 +27,8 @@ function formSubmitHandler(evt) {
     popupClose(evt)
 }
 
+redactBtn.addEventListener('click', popupOpen);
+closeBtn.addEventListener('click', popupClose);
+formElement.addEventListener('submit', formSubmitHandler);
 
 
